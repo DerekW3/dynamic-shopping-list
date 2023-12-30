831aae5936f9b4ff9a546bf8fa7ea9251183f474
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-30 15:03:54 +0100

    Build: Biuld onButtonClick function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+
+function onButtonClick() {
+    let item = input.textContent;
+    input.textContent = "";
+
+    let listItem = document.createElement('li');
+    let itemText = document.createElement('span');
+    let itemDelete = document.createElement('button');
+
+    listItem.appendChild(itemText);
+    listItem.appendChild(itemDelete);
+
+    itemText.textContent = item;
+    itemDelete.textContent = "Delete";
+
+    list.appendChild(listItem);
+
+    itemDelete.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+}
